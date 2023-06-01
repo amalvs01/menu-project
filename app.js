f659a6a30620668b92571d5597286204d0d884ec
Amal s <amalvs01@gmail.com>
2023-06-01 20:57:39 +0530

    filtered using dataset

--- app.js
+++ app.js
@@ -74,24 +74,48 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const filterBtn = document.querySelectorAll('.filter-btn');
 
 window.addEventListener('DOMContentLoaded', function(){
- let displayMenu =menu.map(function(item){
-
-  return `<article class="menu-item">
-            <img src=${item.img} class="photo" alt=${item.title}>
-            <div class="item-info"> 
-              <header>
-                <h4>${item.title}</h4>
-                <h4 class="price">$${item.price}</h4>
-              </header>
-              <p class="item-text">
-              ${item.desc}
-              </p> 
-              </div>
-          </article>`;
+displayMenuItems(menu); 
 });
 
-displayMenu = displayMenu.join("");
-sectionCenter.innerHTML = displayMenu
+filterBtn.forEach(function(btn){
+   btn.addEventListener('click',function(e){
+   const category = e.currentTarget.dataset.id;
+   const menuCategory = menu.filter( function(menuItem){
+    if (menuItem.category === category){
+      return menuItem;
+    } 
+   });
+
+   if(category== 'all'){
+    displayMenuItems(menu)
+   }
+   else{
+    displayMenuItems(menuCategory);
+   }
+  });
 });
+
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
+
+    return`<article class="menu-item">
+              <img src=${item.img} class="photo" alt=${item.title}>
+              <div class="item-info"> 
+                <header>
+                  <h4>${item.title}</h4>
+                  <h4 class="price">$${item.price}</h4>
+                </header>
+                <p class="item-text">
+                ${item.desc}
+                </p> 
+                </div>
+            </article>`;
+  });
+  
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu; 
+};  
